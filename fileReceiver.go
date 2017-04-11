a6286ec83115da2494b49da34695296aa58910a4
vasanth.selvaraj <v-vasanthselvaraj@nbnco.com.au>
2017-04-12 00:47:52 +1000

    first commit

---
+++ fileReceiver.go
@@ -0,0 +1,76 @@
+package main 
+
+import (
+	"fmt"
+	"io"
+	"net"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const BUFFERSIZE = 1024
+
+func main() {
+	//Create a TCP listener on localhost with porth 27001
+	server, err := net.Listen("tcp", "localhost:27001")
+	if err != nil {
+		fmt.Println("Error listetning: ", err)
+		os.Exit(1)
+	}
+	defer server.Close()
+	fmt.Println("Server started! Waiting for connections...")
+	//Spawn a new goroutine whenever a client connects
+	for {
+		connection, err := server.Accept()
+		if err != nil {
+			fmt.Println("Error: ", err)
+			os.Exit(1)
+		}
+		fmt.Println("Client connected")
+		go sendFileToClient(connection)
+	}
+}
+
+func sendFileToClient(connection net.Conn) {
+	fmt.Println("A client has connected!")
+	defer connection.Close()
+	//Create buffer to read in the name and size of the file
+	bufferFileName := make([]byte, 64)
+	bufferFileSize := make([]byte, 10)
+	bufferFilePath := make([]byte, 60)
+	//Get the FilePath
+	connection.Read(bufferFilePath)
+	filePath := strings.Trim(string(bufferFilePath), ":")
+	fmt.Println(filePath)
+	//Get the filesize
+	connection.Read(bufferFileSize)
+	//Strip the ':' from the received size, convert it to a int64
+	fileSize, _ := strconv.ParseInt(strings.Trim(string(bufferFileSize), ":"), 10, 64)
+	//Get the filename
+	connection.Read(bufferFileName)
+	//Strip the ':' once again but from the received file name now
+	fileName := strings.Trim(string(bufferFileName), ":")
+	//Create a new file to write in
+	newFile, err := os.Create(fileName)
+	if err != nil {
+		panic(err)
+	}
+	defer newFile.Close()
+	//Create a variable to store in the total amount of data that we received already
+	var receivedBytes int64
+	//Start writing in the file
+	for {
+		if (fileSize - receivedBytes) < BUFFERSIZE {
+			io.CopyN(newFile, connection, (fileSize - receivedBytes))
+			//Empty the remaining bytes that we don't need from the network buffer
+			connection.Read(make([]byte, (receivedBytes+BUFFERSIZE)-fileSize))
+			//We are done writing the file, break out of the loop
+			break
+		}
+		io.CopyN(newFile, connection, BUFFERSIZE)
+		//Increment the counter
+		receivedBytes += BUFFERSIZE
+	}
+	fmt.Println("Received file completely!")
+}
---
+++ sendFile.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net"
+	"os"
+	"strconv"
+//	"strings"
+)
+
+//Define that the binairy data of the file will be sent 1024 bytes at a time
+const BUFFERSIZE = 1024
+
+func main() {
+	connection, err := net.Dial("tcp", "localhost:27001")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Connected to server, start receiving the file name and file size")
+	fmt.Println("A client has connected!")
+
+	//Open the file that needs to be send to the client
+	file, err := os.Open("dummyfile.dat")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+	//Get the filename and filesize
+	fileInfo, err := file.Stat()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fileSize := fillString(strconv.FormatInt(fileInfo.Size(), 10), 10)
+	fileName := fillString(fileInfo.Name(), 64)
+	filePath := fillString("/tmp",60)
+	//Send the file header first so the client knows the filename and how long it has to read the incomming file
+	fmt.Println("Sending filename and filesize!")
+	fmt.Println(fileSize)
+	fmt.Println(fileName)
+	fmt.Println(filePath)
+	//Write first 10 bytes to client telling them the filesize
+	connection.Write([]byte(fileSize))
+	//Write 64 bytes to client containing the filename
+	connection.Write([]byte(fileName))
+	connection.Write([]byte(filePath))
+	//Initialize a buffer for reading parts of the file in
+	sendBuffer := make([]byte, BUFFERSIZE)
+	//Start sending the file to the client
+	fmt.Println("Start sending file!")
+	for {
+		_, err = file.Read(sendBuffer)
+		if err == io.EOF {
+			//End of file reached, break out of for loop
+			break
+		}
+		connection.Write(sendBuffer)
+	}
+	fmt.Println("File has been sent, closing connection!")
+	defer connection.Close()
+}
+
+//This function is to 'fill'
+func fillString(retunString string, toLength int) string {
+	for {
+		lengtString := len(retunString)
+		if lengtString < toLength {
+			retunString = retunString + ":"
+			continue
+		}
+		break
+	}
+	return retunString
+}
+
